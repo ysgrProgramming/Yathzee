45d1ecaabde1418b01ac05391fc4b8c34655f9d2
ysgrProgramming <ymizutani_1205@magnate2022.localdomain>
2022-10-05 01:40:42 +0900

    add cmake files

--- solver/c++/pymodule.cpp
+++
@@ -1,9 +0,0 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include "disp_vector.h"
-
-PYBIND11_MODULE( mymodule, m )
-{
-   m.doc() = "mymodule";
-   m.def( "disp_vec", &dispVector, "display vector" );
-}
